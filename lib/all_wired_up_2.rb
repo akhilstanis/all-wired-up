648b382ea255f1910ebbc9c2a754beb66eac2f69
akhil stanislavose <akhil@mobme.in>
2012-01-04 17:11:59 +0530

    Need to test complex circuits

---
+++ lib/all_wired_up_2.rb
@@ -0,0 +1,102 @@
+class AllWiredUp2
+
+  attr_accessor :circuit
+
+  # if /\s*[01]-*@/ replace it with ! or *
+  # /[01]-*|/ replace with operand above operator
+
+  # c[x][y] = index of /[01]/
+  # while c[x][y] != " " or nil
+  #   if c[x][y] =~ /[AOXN]/
+  #     operator_index = y
+  #   gates << c[x][y]
+  #   c[x][y] = " "
+  #   y++
+  # end
+  # c[x][opeartor_index] = out(gates)
+
+  # delete empty lines
+
+  def initialize path_to_circuit_file
+    File.open path_to_circuit_file do |file|
+      @circuit = file.readlines
+    end
+  end
+
+  def get_rid_of_wires optional_circuit=nil
+    circuit = optional_circuit || @circuit
+    @circuit = circuit.collect do |line|
+      bulb_match = line.match /\s*(?<operand>[01])-*[@]$/
+      if bulb_match
+        bulb_match['operand'] == "0" ? "OFF\n" : "ON\n"
+      else
+        match = line.match /\s*(?<operand>[01])-*[|]/
+        if match
+          line.sub /\s*[01]-*[|]/, " " * line.index('|') + match['operand']
+        else
+          line
+        end
+      end
+    end
+  end
+
+  def gate_out stage
+    stage.delete '|'
+    case
+    when stage.include?('O')
+      stage.include?('1') ? '1' : '0'
+    when stage.include?('A')
+      stage.include?('0') ? '0' : '1'
+    when stage.include?('N')
+      stage.delete 'N'
+      stage.first == '0' ? '1' : '0'
+    when stage.include?('X')
+      ones = stage.select { |input| input == '1' }
+      ones.count % 2 == 0 ? '0' : '1'
+    end
+  end
+
+  def get_rid_of_gates optional_circuit=nil
+    circuit = optional_circuit || @circuit
+    skip_rows = []
+    circuit.each_index do |index|
+      next if skip_rows.include? index
+      operand_index = circuit[index].index /[01]/
+      if operand_index
+        row = index
+        gate = []
+        operator_row = nil
+        until circuit[row].nil? || ([" ", nil].include? circuit[row][operand_index]) do
+          operator_row = row if circuit[row][operand_index] =~ /[XOAN]/
+          gate.push circuit[row][operand_index]
+          circuit[row][operand_index] = " "
+          skip_rows << row
+          row += 1
+        end
+        circuit[operator_row][operand_index] = gate_out gate
+      end
+    end
+    @circuit = circuit
+    # circuit.delete_if { |line| line =~ /^[\s]+\n$/ }
+  end
+
+  def any_more_bulbs?
+    ret = false
+    circuit = @circuit
+    circuit.each do |line|
+      ret = true if line =~ /-*@$/
+    end
+    pp @circuit Some probelm here modifying @circuit
+    ret
+  end
+
+  def process
+
+    while any_more_bulbs? do
+      get_rid_of_wires
+      get_rid_of_gates
+    end
+    @circuit
+  end
+
+end
---
+++ spec/all_wired_up_2_spec.rb
@@ -0,0 +1,112 @@
+require 'spec_helper'
+require 'all_wired_up_2'
+require 'pp'
+
+describe AllWiredUp2 do
+
+  before :each do
+    @o = AllWiredUp2.new(File.expand_path(File.dirname(__FILE__)) + '/fixtures/simple_circuits.txt')
+  end
+
+  # it "should get rid of wires" do
+
+  #   @o.get_rid_of_wires.should == [
+  #     "              0\n",
+  #     "              |\n",
+  #     "              O-----------@\n",
+  #     "              1\n",
+  #     "\n",
+  #     "              0\n",
+  #     "              A------------|\n",
+  #     "              1            |\n",
+  #     "                           X------------@\n",
+  #     "              1            |\n",
+  #     "              N------------|\n"
+  #   ]
+
+  #   simple_level_2 = [
+  #     "              1-----------@\n",
+  #     "\n",
+  #     "              0------------|\n",
+  #     "                           |\n",
+  #     "                           X------------@\n",
+  #     "                           |\n",
+  #     "              0------------|\n"
+  #   ]
+
+  #   @o.get_rid_of_wires(simple_level_2).should == [
+  #     "ON\n",
+  #     "\n",
+  #     "                           0\n",
+  #     "                           |\n",
+  #     "                           X------------@\n",
+  #     "                           |\n",
+  #     "                           0\n"
+  #   ]
+
+  #   simple_level_3 = [
+  #     "ON\n",
+  #     "\n",
+  #     "                           0------------@\n",
+  #   ]
+
+  #   @o.get_rid_of_wires(simple_level_3).should == [
+  #     "ON\n",
+  #     "\n",
+  #     "OFF\n",
+  #   ]
+
+  # end
+
+  # it "should get rid of gates" do
+
+  #   level_0 = [
+  #     "              0\n",
+  #     "              |\n",
+  #     "              O-----------@\n",
+  #     "              1\n",
+  #     "\n",
+  #     "              0\n",
+  #     "              A------------|\n",
+  #     "              1            |\n",
+  #     "                           X------------@\n",
+  #     "              1            |\n",
+  #     "              N------------|\n"
+  #   ]
+
+  #   @o.get_rid_of_gates(level_0).should == [
+  #     "              1-----------@\n",
+  #     "\n",
+  #     "              0------------|\n",
+  #     "                           |\n",
+  #     "                           X------------@\n",
+  #     "                           |\n",
+  #     "              0------------|\n"
+  #   ]
+
+  #   level_1 = [
+  #     "ON\n",
+  #     "\n",
+  #     "                           0\n",
+  #     "                           |\n",
+  #     "                           X------------@\n",
+  #     "                           |\n",
+  #     "                           0\n"
+  #   ]
+
+  #   @o.get_rid_of_gates(level_1).should == [
+  #     "ON\n",
+  #     "\n",
+  #     "                           0------------@\n"
+  #   ]
+
+  # end
+
+  it "should process a circuit file" do
+    # @o.process.should == ["ON\n", "\n", "OFF\n"]
+    x = AllWiredUp2.new(File.expand_path(File.dirname(__FILE__)) + '/fixtures/complex_circuits.txt')
+    x.process.should == []
+  end
+
+
+end
